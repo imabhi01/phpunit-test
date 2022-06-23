4ec3611abcf68bc80b78a211a7c8ba96bea521f5
imabhi01 <iamdev.abhishek@gmail.com>
2022-06-23 13:47:18 +0545

    main

--- tests/ValidatePasswordTest.php
+++ tests/ValidatePasswordTest.php
@@ -7,6 +7,8 @@ class ValidatePasswordTest extends TestCase{
     public function testValidLength(){
         $valPass = new ValidatePassword();
         $this->assertTrue($valPass->validLength('123123'));
+        $this->assertTrue($valPass->validLength('12'));
+        $this->assertTrue($valPass->validLength('12512512612612512412412412'));
     }
 
 }
